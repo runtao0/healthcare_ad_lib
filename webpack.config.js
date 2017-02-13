68cbfc68a0098d2a6ca32fefb84320fb851203e8
Runtao0 <ry2228@columbia.edu>
2017-02-12 23:50:12 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './lib/main.js',
+  output: {
+    path: './lib',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js']
+  },
+  // debug: true,
+};
